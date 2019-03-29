a9a28b1867f93b108fa8c6daa29638d9ae3fe9b0
feifeiyuan <33796779+feifeiyuan@users.noreply.github.com>
2019-03-29 10:24:03 +0800

    Create q_learning.h

---
+++ q_learning.h
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <iomanip>
+#include <time.h>
+#include <unistd.h>
+#define random(x) rand()%x;
+
+using namespace std;
+
+#define SCHDULE_SIZE 4
+
+static const char *schedlable[] = {"0", "-20", "-40", "-60", "-80"};
+static const char *schedtune[SCHDULE_SIZE] = {
+	"/dev/stune/schedtune.boost",
+    "/dev/stune/top-app/schedtune.boost",
+    "/dev/stune/foreground/schedtune.boost",
+    "/dev/stune/background/schedtune.boost"
+    };
+
+/*
+class States{
+public:
+	States():mLitePower(0), mBigPower(0), mFPS(0){}
+private:
+	int mLitePower;
+	int mBigPower;
+	int mFPS;
+};
+*/
+
+class q_learning{
+public:
+	q_learning():mEpsilon(0.9), mLr(0.1), mGamma(0.9), mMaxEpisodes(5),\
+	mSampleTime(100*1000), mStepCounter(0), mStates(10), mActions(5){}
+	void setLable(int lable_index);
+	void build_q_table();
+	void print_q_table();
+	int choose_action(int state);
+	vector<int> get_env_feedback(int state);
+	void update_env(int state, int episode);
+	void rl();
+	float find_max(int state, int *index=NULL);
+	int get_input_data();
+private:
+	vector< vector<float> > mQtable;
+	float mEpsilon;
+	float mLr;
+	float mGamma;
+	long int mStepCounter;
+	int  mStates;
+	int  mActions;
+	long int mSampleTime;
+	long int mMaxEpisodes;
+};
